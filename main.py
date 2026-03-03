a1be062ba515e726b98a8bf12cc72d6180c70686
Jenushan <jenushan349@gmail.com>
2026-03-03 17:02:54 -0500

    feat: add basic CLI argument validation for add command

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Task Tracker")
+import sys # Provides information about python interpreter 
+
+args = sys.argv
+
+if len(args) < 2: 
+  print('Please provide a valid command')
+  sys.exit() # Immediatley ends the script
+  
+if args[1] != 'add': 
+  print("Please provide a valid command")
+  sys.exit()
+else:
+  if len(args) < 3:
+    print("Please provide a description")
+    sys.exit()
+  else:  
+    print(f"Adding task: {args[2]}")
+    
