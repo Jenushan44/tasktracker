187671f61d87150cb5beb22a8a37b9146551a066
Jenushan <jenushan349@gmail.com>
2026-03-04 19:40:40 -0500

    feat: create and load tasks.json storage

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import sys # Provides information about python interpreter 
+import json 
+import datetime 
+from pathlib import Path 
+
+tasks_path = Path("tasks.json")
 
 args = sys.argv
 
 if len(args) < 2: 
   print('Please provide a valid command')
   sys.exit() # Immediatley ends the script
-  
+    
 if args[1] != 'add': 
   print("Please provide a valid command")
   sys.exit()
@@ -15,4 +20,16 @@ else:
     sys.exit()
   else:  
     print(f"Adding task: {args[2]}")
+
+
+if tasks_path.exists(): 
+  
+  with open("tasks.json", "r") as file: 
+    data = json.load(file)
+
+else: 
+  data = { "nextId": 1, "tasks": [] }
+  with open("tasks.json", "w") as file: 
+    json.dump(data,file)
+  
     
