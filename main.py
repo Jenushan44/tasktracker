5dcfbe58825eeef0a7e919d12d5e8035c4da3565
Jenushan <jenushan349@gmail.com>
2026-03-05 21:26:15 -0500

    feat: implement list command to display tasks

--- main.py
+++ main.py
@@ -11,17 +11,6 @@ if len(args) < 2:
   print('Please provide a valid command')
   sys.exit() # Immediatley ends the script
     
-if args[1] != 'add': 
-  print("Please provide a valid command")
-  sys.exit()
-else:
-  if len(args) < 3:
-    print("Please provide a description")
-    sys.exit()
-  else:  
-    print(f"Adding task: {args[2]}")
-
-
 if tasks_path.exists(): 
   with open("tasks.json", "r") as file: 
     data = json.load(file)
@@ -30,20 +19,33 @@ else:
   with open("tasks.json", "w") as file: 
     json.dump(data,file)
   
-    
-description = args[2]
-task_id = data["nextId"]
+if args[1] == 'add': 
+  description = args[2]
+  task_id = data["nextId"]
 
-currentTask = {
-  "id": task_id, 
-  "description": description, 
-  "status": "todo",
-}
+  currentTask = {
+    "id": task_id, 
+    "description": description, 
+    "status": "todo",
+  }
 
-data["tasks"].append(currentTask)
-data["nextId"] += 1 
+  data["tasks"].append(currentTask)
+  data["nextId"] += 1 
 
-with open("tasks.json", "w") as file: 
-  json.dump(data, file)
+  with open("tasks.json", "w") as file: 
+    json.dump(data, file)
 
-print(f"Tasks successfully added {task_id}")
+  print(f"Tasks successfully added {task_id}")
+
+elif args[1] == 'list': 
+  with open("tasks.json", "r") as file: 
+    d = json.load(file)
+
+  for task in data['tasks']: 
+    print(task["id"], end= " ")
+    print(task["description"], end=" ")
+    print(task["status"])
+
+else: 
+  print("Please provide a valid command")
+  sys.exit()
